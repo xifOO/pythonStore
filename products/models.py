f1af0e7ec405d5704b6b02f333426d8c33d13871
xifOO <wedyi28111@gmail.com>
2022-10-03 21:53:45 +0300

    change product models

--- products/models.py
+++ products/models.py
@@ -10,40 +10,10 @@ class Category(models.Model):
         return f"{self.name}"
 
 
-class Phone(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Category")
-    image = models.ImageField(upload_to="products", blank=False, null=False, verbose_name="phone_image")
-    description = models.TextField(blank=False, null=False)
-    slug = AutoSlugField(populate_from="name")
-    price = models.PositiveIntegerField(blank=False, null=False)
-
-    def __str__(self):
-        return f"{self.name}"
-
-    def get_absolute_url(self):
-        return reverse('acticle_detail', kwargs={'slug': self.slug})
-
-
-class Notebook(models.Model):
-    name = models.CharField(max_length=255, blank=False, null=False)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Category")
-    image = models.ImageField(upload_to="products", blank=False, null=False, verbose_name="notebook_image")
-    description = models.TextField(blank=False, null=False)
-    slug = AutoSlugField(populate_from="name")
-    price = models.PositiveIntegerField(blank=False, null=False)
-
-    def __str__(self):
-        return f"{self.name}"
-
-    def get_absolute_url(self):
-        return reverse('acticle_detail', kwargs={'slug': self.slug})
-
-
-class Tablet(models.Model):
-    name = models.CharField(max_length=255, blank=False, null=False)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Category")
-    image = models.ImageField(upload_to="products", blank=False, null=False, verbose_name="tablet_image")
+    image = models.ImageField(upload_to="products", blank=False, null=False, verbose_name="product_image")
     description = models.TextField(blank=False, null=False)
     slug = AutoSlugField(populate_from="name")
     price = models.PositiveIntegerField(blank=False, null=False)
