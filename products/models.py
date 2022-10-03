8445fadb1037c082699d500f92997aa9b9bfdfed
xifOO <wedyi28111@gmail.com>
2022-10-03 19:58:08 +0300

    change product models

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from autoslug import AutoSlugField
 
 
@@ -12,31 +13,43 @@ class Category(models.Model):
 class Phone(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Category")
+    image = models.ImageField(upload_to="products", blank=False, null=False, verbose_name="phone_image")
     description = models.TextField(blank=False, null=False)
     slug = AutoSlugField(populate_from="name")
-    price = models.PositiveSmallIntegerField(blank=False, null=False)
+    price = models.PositiveIntegerField(blank=False, null=False)
 
     def __str__(self):
         return f"{self.name}"
+
+    def get_absolute_url(self):
+        return reverse('acticle_detail', kwargs={'slug': self.slug})
 
 
 class Notebook(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Category")
+    image = models.ImageField(upload_to="products", blank=False, null=False, verbose_name="notebook_image")
     description = models.TextField(blank=False, null=False)
     slug = AutoSlugField(populate_from="name")
-    price = models.PositiveSmallIntegerField(blank=False, null=False)
+    price = models.PositiveIntegerField(blank=False, null=False)
 
     def __str__(self):
         return f"{self.name}"
+
+    def get_absolute_url(self):
+        return reverse('acticle_detail', kwargs={'slug': self.slug})
 
 
 class Tablet(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Category")
+    image = models.ImageField(upload_to="products", blank=False, null=False, verbose_name="tablet_image")
     description = models.TextField(blank=False, null=False)
     slug = AutoSlugField(populate_from="name")
-    price = models.PositiveSmallIntegerField(blank=False, null=False)
+    price = models.PositiveIntegerField(blank=False, null=False)
 
     def __str__(self):
         return f"{self.name}"
+
+    def get_absolute_url(self):
+        return reverse('acticle_detail', kwargs={'slug': self.slug})
