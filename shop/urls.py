177a83d80f3d99996198e9b080f642c4280b1a3a
xifOO <wedyi28111@gmail.com>
2022-10-03 19:56:34 +0300

    add mailing urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path("", views.home_page, name="home_page"),
+    path("logout/", views.logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name="logout"),
 ]
