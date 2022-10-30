4220c79b82cfd24455effc0145f74d6f1ae978f0
xifOO <wedyi28111@gmail.com>
2022-10-31 00:33:39 +0300

    shop urls

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,4 @@ from . import views
 
 urlpatterns = [
     path("", views.home_page, name="home_page"),
-    path("logout/", views.logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name="logout"),
 ]
