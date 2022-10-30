efcacaa1aae5caaa366233b37c190b7949f514c9
xifOO <wedyi28111@gmail.com>
2022-10-31 00:32:28 +0300

    cart urls

---
+++ cart/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.cart_detail, name="cart"),
+    path('clear', views.cart_clear, name="clear_cart"),
+    path('add/<int:product_id>', views.cart_add, name="add_to_cart"),
+    path('remove/<int:product_id>', views.cart_remove, name="remove_from_cart"),
+]
