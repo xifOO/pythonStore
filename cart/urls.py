dcc090f4ee1c55d18e3a36d18e6dbc55e6933c6f
xifOO <wedyi28111@gmail.com>
2022-11-04 19:47:24 +0300

    rename url field

--- cart/urls.py
+++ cart/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.cart_detail, name="cart"),
     path('clear', views.cart_clear, name="clear_cart"),
-    path('add/<int:product_id>', views.cart_add, name="add_to_cart"),
-    path('remove/<int:product_id>', views.cart_remove, name="remove_from_cart"),
+    path('add/<int:id>', views.cart_add, name="add_to_cart"),
+    path("remove/<int:id>", views.cart_remove, name="remove_from_cart")
 ]
